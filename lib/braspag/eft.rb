37a6c85b903a9582f6a721d2cd52390ed9b5c640
Renato Elias <renato.elias@gmail.com>
2011-06-29 18:14:11 -0300

    Gerando formulario

---
+++ lib/braspag/eft.rb
@@ -0,0 +1,88 @@
+module Braspag
+  class Eft
+    MAPPING = {
+      :merchant_id => "Id_Loja",
+      :order_id => "VendaId",
+      :customer_name => "Nome",
+      :customer_id => "Cpf",
+      :amount => "Valor",
+      :payment_method => "codpagamento",
+      :installments => "PARCELAS",
+      :has_interest => "TIPOPARCELADO"
+    }
+
+    def initialize(connection, params)
+      raise InvalidConnection unless connection.is_a?(Braspag::Connection)
+
+      @connection = connection
+      @params = params
+      @params[:merchant_id] = connection.merchant_id
+      ok?
+    end
+
+    def ok?
+      raise IncompleteParams if @params[:order_id].nil? || @params[:amount].nil? || @params[:payment_method].nil?
+
+      raise InvalidOrderId unless @params[:order_id].is_a?(String) || @params[:order_id].is_a?(Fixnum)
+      raise InvalidOrderId unless (1..50).include?(@params[:order_id].to_s.size)
+
+      unless @params[:customer_name].nil?
+        raise InvalidCustomerName unless (1..255).include?(@params[:customer_name].to_s.size)
+      end
+
+      unless @params[:customer_id].nil?
+        raise InvalidCustomerId unless (11..18).include?(@params[:customer_id].to_s.size)
+      end
+
+      unless @params[:installments].nil?
+        raise InvalidInstallments unless (1..2).include?(@params[:installments].to_s.size)
+        begin
+          @params[:installments] = Integer(@params[:installments]) unless @params[:installments].is_a?(Integer)
+        rescue Exception => e
+          raise InvalidInstallments
+        end
+      end
+
+      unless @params[:has_interest].nil?
+        raise InvalidHasInterest unless (@params[:has_interest].is_a?(TrueClass) || @params[:has_interest].is_a?(FalseClass))
+      end
+
+      
+      true
+    end
+   
+   def generate
+     data = {}
+     @params.each {|name, value|
+       if MAPPING[name].nil?
+         data[name] = value
+       else
+         data[MAPPING[name]] = value
+       end
+     }
+     
+     html = "<form id=\"form_tef_#{@params[:order_id]}\" name=\"form_tef_#{@params[:order_id]}\" action=\"#{self.uri}\" method=\"post\">\n"
+     
+     data.each do |key, value|
+       html.concat "<input type=\"text\" name=\"#{key}\" value=\"#{value}\" />\n"
+     end
+    
+     html.concat <<-EOHTML
+      </form>
+       <script type="text/javascript" charset="utf-8">
+         document.forms["form_tef_#{@params[:order_id]}"].submit();
+       </script>
+     EOHTML
+     
+     html
+   end
+   
+   protected
+
+   def uri
+     "#{@connection.base_url}/pagador/passthru.asp"
+   end
+   
+    
+  end
+end
---
+++ spec/eft_spec.rb
@@ -0,0 +1,178 @@
+#encoding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Braspag::Eft do
+
+  let!(:merchant_id) {"{84BE7E7F-698A-6C74-F820-AE359C2A07C2}"}
+  let!(:crypto_key) {"{84BE7E7F-698A-6C74-F820-AE359C2A07C2}"}
+  let!(:connection) {Braspag::Connection.new(merchant_id, :test, crypto_key)}
+  
+  describe ".new" do
+
+     it "should raise an error when no connection is given" do
+       expect {
+         Braspag::Eft.new("", {})
+       }.to raise_error(Braspag::InvalidConnection)
+     end
+
+     it "should raise an error when :order_id is not present" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :amount => "10000",
+           :payment_method => "11"
+         })
+       }.to raise_error(Braspag::IncompleteParams)
+     end
+
+     it "should raise an error when :amount is not present" do
+        expect {
+          Braspag::Eft.new(connection, {
+            :order_id => "12",
+            :payment_method => "11"
+          })
+        }.to raise_error(Braspag::IncompleteParams)
+     end
+
+     it "should raise an error when :payment_method is not present" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "13",
+           :amount => "12000"
+         })
+       }.to raise_error(Braspag::IncompleteParams)
+     end
+
+     it "should raise an error when :order_id is less than 1 character" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "",
+           :amount => "12300",
+           :payment_method => "11"
+         })
+       }.to raise_error(Braspag::InvalidOrderId)
+     end
+
+     it "should raise an error when :order_id is more than 50 characters" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "1" * 51,
+           :amount => "1200",
+           :payment_method => "11"
+         })
+       }.to raise_error(Braspag::InvalidOrderId)
+     end
+
+     it "should raise an error when :customer_name is less than 1 character" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "102",
+           :amount => "4200",
+           :payment_method => "11",
+           :customer_name => ""
+         })
+       }.to raise_error(Braspag::InvalidCustomerName)
+     end
+
+     it "should raise an error when :customer_name is more than 255 characters" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "112",
+           :amount => "12100",
+           :payment_method => "11",
+           :customer_name => "A" * 256
+         })
+       }.to raise_error(Braspag::InvalidCustomerName)
+     end
+
+     it "should raise an error when :customer_id is less than 11 characters" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "23",
+           :amount => "25100",
+           :payment_method => "11",
+           :customer_id => "2" * 10
+         })
+       }.to raise_error(Braspag::InvalidCustomerId)
+     end
+
+     it "should raise an error when :customer_id is more than 18 characters" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "90",
+           :amount => "9000",
+           :payment_method => "11",
+           :customer_id => "3" * 19
+         })
+       }.to raise_error(Braspag::InvalidCustomerId)
+     end
+
+     it "should raise an error when :installments is less than 1 character" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "900",
+           :amount => "9200",
+           :payment_method => "11",
+           :installments => ""
+         })
+       }.to raise_error(Braspag::InvalidInstallments)
+     end
+
+     it "should raise an error when :installments is more than 2 characters" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "91",
+           :amount => "8000",
+           :payment_method => "11",
+           :installments => "5" * 3
+         })
+       }.to raise_error(Braspag::InvalidInstallments)
+     end
+
+    it "should raise an error when :installments is not a number" do
+        expect {
+          Braspag::Eft.new(connection, {
+            :order_id => "91",
+            :amount => "8000",
+            :payment_method => "11",
+            :installments => "A" * 2
+          })
+        }.to raise_error(Braspag::InvalidInstallments)
+    end
+
+     it "should raise an error when :has_interest is not boolean" do
+       expect {
+         Braspag::Eft.new(connection, {
+           :order_id => "76",
+           :amount => "5000",
+           :payment_method => "11",
+           :has_interest => []
+         })
+       }.to raise_error(Braspag::InvalidHasInterest)
+     end
+   end
+
+  describe ".generate" do
+    context "with correct data" do
+      it {
+        html = <<-EOHTML
+<form id="form_tef_1234123125" name="form_tef_1234123125" action="https://homologacao.pagador.com.br/pagador/passthru.asp" method="post">
+<input type="text" name="VendaId" value="1234123125" />
+<input type="text" name="Valor" value="12300" />
+<input type="text" name="codpagamento" value="11" />
+<input type="text" name="Id_Loja" value="{84BE7E7F-698A-6C74-F820-AE359C2A07C2}" />
+      </form>
+       <script type="text/javascript" charset="utf-8">
+         document.forms["form_tef_1234123125"].submit();
+       </script>
+        EOHTML
+
+        Braspag::Eft.new(connection , {
+          :order_id => "1234123125",
+          :amount => "12300",
+          :payment_method => "11"
+        }).generate.should == html
+     }
+    end
+   end
+
+end
