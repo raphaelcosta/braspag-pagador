3e01e8592e49bd357580b75ab343f766e689004b
Marcelo Linhares <marcelo@marcelo-linhares.(none)>
2011-07-08 15:08:05 -0300

    cobertura de teste pra cartao

---
+++ lib/rbraspag/utils.rb
@@ -0,0 +1,10 @@
+module Braspag
+
+  class Utils
+    def self.convert_decimal_to_string(value)
+      cents = "0#{((value - value.to_i) * 100).to_i}".slice(-2,2)
+      integer = (value - (value - value.to_i)).to_i
+      "#{integer},#{cents}"
+    end
+  end
+end
