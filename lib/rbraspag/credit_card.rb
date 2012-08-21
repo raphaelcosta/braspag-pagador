9247d6e12fe51825ddd69a9b415fac0a52d37882
Daniel Konishi <dkonishi@gmail.com>
2012-08-21 11:34:06 -0300

    Extracted the 'CartaoProtegido' web service calls to separated class: ProtectedCreditCard

--- lib/rbraspag/credit_card.rb
+++ lib/rbraspag/credit_card.rb
@@ -1,39 +1,6 @@
 module Braspag
   class CreditCard < PaymentMethod
 
-    PAYMENT_METHODS = {
-      # BRASIL
-      :amex_2p                  => 18,  # American Express 2 Party
-      :cielo_noauth_visa        => 71,  # Cielo webservice captura automática sem autenticação - Visa
-      :cielo_preauth_visa       => 73,  # Cielo webservice preauth sem autenticação - Visa
-      :cielo_noauth_mastercard  => 120, # Cielo webservice captura automática sem autenticação - Mastercard
-      :cielo_preauth_mastercard => 122, # Cielo webservice preauth sem autenticação - Mastercard
-      :cielo_noauth_elo         => 126, # Cielo webservice captura automática sem autenticação - ELO
-      :cielo_noauth_diners      => 130, # Cielo webservice captura automática sem autenticação - Diners
-      :redecard                 => 20,  # Redecard Mastercard/Diners/Visa
-      :redecard_preauth         => 42,  # Redecard preauth Mastercard/Diners/Visa
-      :cielo_sitef              => 57,  # Cielo SITEF
-      :hipercard_sitef          => 62,  # Hipercard SITEF
-      :hipercard_moip           => 90,  # Hipercard MOIP
-      :oi_paggo                 => 55,  # OiPaggo
-      :amex_sitef               => 58,  # Amex SITEF
-      :aura_dtef                => 37,  # Aura DTEF
-      :redecard_sitef           => 44,  # Redecard SITEF - Mastercard/Diners
-      # MÉXICO
-      :mex_amex_2p            => 45, # American Express 2 Party
-      :mex_banorte_visa       => 50, # Banorte Visa
-      :mex_banorte_diners     => 52, # Banorte Diners
-      :mex_banorte_mastercard => 53, # Banorte Mastercard
-      # COLÔMBIA
-      :col_visa   => 63, # Visa
-      :col_amex   => 65, # Amex
-      :col_diners => 66, # Diners
-      # INTERNACIONAL
-      :paypal_express => 35, # PayPal Express Checkout
-      # HOMOLOGATION
-      :braspag => 997
-    }
-
     MAPPING = {
       :merchant_id => "merchantId",
       :order => 'order',
@@ -49,35 +16,9 @@ module Braspag
       :type => "typePayment",
     }
 
-    PROTECTED_CARD_MAPPING = {
-      :request_id => "RequestId",
-      :merchant_id => "MerchantKey",
-      :customer_name => "CustomerName",
-      :holder => "CardHolder",
-      :card_number => "CardNumber",
-      :expiration => "CardExpiration"
-    }
-
-    JUST_CLICK_MAPPING = {
-      :request_id => "RequestId",
-      :merchant_id => "MerchantKey",
-      :customer_name => "CustomerName",
-      :order_id => "OrderId",
-      :amount => "Amount",
-      :payment_method => "PaymentMethod",
-      :number_installments => "NumberInstallments",
-      :payment_type => "PaymentType",
-      :just_click_key => "JustClickKey",
-      :security_code => "SecurityCode"
-    }
-
     AUTHORIZE_URI = "/webservices/pagador/Pagador.asmx/Authorize"
     CAPTURE_URI = "/webservices/pagador/Pagador.asmx/Capture"
     CANCELLATION_URI = "/webservices/pagador/Pagador.asmx/VoidTransaction"
-    SAVE_PROTECTED_CARD_URI = "/CartaoProtegido.asmx?wsdl"
-    GET_PROTECTED_CARD_URI = "/CartaoProtegido.asmx/GetCreditCard"
-    JUST_CLICK_SHOP_URI = "/CartaoProtegido.asmx?wsdl"
-
     PRODUCTION_INFO_URI   = "/webservices/pagador/pedido.asmx/GetDadosCartao"
     HOMOLOGATION_INFO_URI = "/pagador/webservice/pedido.asmx/GetDadosCartao"
 
@@ -99,10 +40,7 @@ module Braspag
         end
       end
 
-      request = ::HTTPI::Request.new self.authorize_url
-      request.body = data
-
-      response = Braspag::Poster.new(request).do_post(:authorize, data)
+      response = Braspag::Poster.new(self.authorize_url).do_post(:authorize, data)
 
       Utils::convert_to_map(response.body, {
           :amount => nil,
@@ -125,10 +63,7 @@ module Braspag
         MAPPING[:merchant_id] => merchant_id
       }
 
-      request = ::HTTPI::Request.new(self.capture_url)
-      request.body = data
-
-      response = Braspag::Poster.new(request).do_post(:capture, data)
+      response = Braspag::Poster.new(self.capture_url).do_post(:capture, data)
 
       Utils::convert_to_map(response.body, {
           :amount => nil,
@@ -151,10 +86,7 @@ module Braspag
         MAPPING[:merchant_id] => merchant_id
       }
 
-      request = ::HTTPI::Request.new(self.cancellation_url)
-      request.body = data
-
-      response = Braspag::Poster.new(request).do_post(:void, data)
+      response = Braspag::Poster.new(self.cancellation_url).do_post(:void, data)
 
       Utils::convert_to_map(response.body, {
           :amount => nil,
@@ -166,88 +98,13 @@ module Braspag
         })
     end
 
-    # saves credit card in Braspag PCI Compliant
-    def self.save(params = {})
-      connection = Braspag::Connection.instance
-      params[:merchant_id] = connection.merchant_id
-
-      self.check_protected_card_params(params)
-
-      data = { 'saveCreditCardRequestWS' => {} }
-
-      PROTECTED_CARD_MAPPING.each do |k, v|
-        data['saveCreditCardRequestWS'][v] = params[k] || ""
-      end
-
-      client = Savon::Client.new(self.save_protected_card_url)
-      response = client.request(:web, :save_credit_card) do
-        soap.body = data
-      end
-
-      response.to_hash[:save_credit_card_response][:save_credit_card_result]
-
-    end
-
-    # request the credit card info in Braspag PCI Compliant
-    def self.get(just_click_key)
-      connection = Braspag::Connection.instance
-
-      raise InvalidJustClickKey unless valid_just_click_key?(just_click_key)
-
-      request = ::HTTPI::Request.new(self.get_protected_card_url)
-      request.body = { 'getCreditCardRequestWS' => {:loja => connection.merchant_id, :justClickKey => just_click_key} }
-
-      response = ::HTTPI.post(request)
-
-      response = Utils::convert_to_map(response.body, {
-          :holder => "CardHolder",
-          :card_number => "CardNumber",
-          :expiration => "CardExpiration",
-          :masked_card_number => "MaskedCardNumber"
-        })
-
-      raise UnknownError if response[:card_number].nil?
-      response
-    end
-
-    def self.just_click_shop(params = {})
-      connection = Braspag::Connection.instance
-      params[:merchant_id] = connection.merchant_id
-
-      self.check_just_click_shop_params(params)
-
-      order_id = params[:order_id]
-      raise InvalidOrderId unless self.valid_order_id?(order_id)
-
-      data = { 'justClickShopRequestWS' => {} }
-
-      JUST_CLICK_MAPPING.each do |k, v|
-        case k
-        when :payment_method
-          data['justClickShopRequestWS'][v] = Braspag::Connection.instance.homologation? ? PAYMENT_METHODS[:braspag] : PAYMENT_METHODS[params[:payment_method]]
-        else
-          data['justClickShopRequestWS'][v] = params[k] || ""
-        end
-      end
-
-      client = Savon::Client.new(self.just_click_shop_url)
-      response = client.request(:web, :just_click_shop) do
-        soap.body = data
-      end
-
-      response.to_hash[:just_click_shop_response][:just_click_shop_result]
-
-    end
-
     def self.info(order_id)
       connection = Braspag::Connection.instance
 
       raise InvalidOrderId unless self.valid_order_id?(order_id)
 
-      request = ::HTTPI::Request.new(self.info_url)
-      request.body = {:loja => connection.merchant_id, :numeroPedido => order_id.to_s}
-
-      response = ::HTTPI.post(request)
+      data = {:loja => connection.merchant_id, :numeroPedido => order_id.to_s}
+      response = Braspag::Poster.new(self.info_url).do_post(:info_credit_card, data)
 
       response = Utils::convert_to_map(response.body, {
           :checking_number => "NumeroComprovante",
@@ -286,41 +143,22 @@ module Braspag
       raise InvalidNumberPayments if params[:number_payments].to_i < 1 || params[:number_payments].to_i > 99
     end
 
-    def self.check_protected_card_params(params)
-      [:request_id, :customer_name, :holder, :card_number, :expiration].each do |param|
-        raise IncompleteParams if params[param].nil?
-      end
-
-      raise InvalidHolder if params[:holder].to_s.size < 1 || params[:holder].to_s.size > 100
-
-      matches = params[:expiration].to_s.match /^(\d{2})\/(\d{2,4})$/
-      raise InvalidExpirationDate unless matches
-      begin
-        year = matches[2].to_i
-        year = "20#{year}" if year.size == 2
-
-        Date.new(year.to_i, matches[1].to_i)
-      rescue ArgumentError
-        raise InvalidExpirationDate
-      end
+    # <b>DEPRECATED:</b> Please use <tt>ProtectedCreditCard.save</tt> instead.
+    def self.save(params)
+      warn "[DEPRECATION] `CreditCard.save` is deprecated.  Please use `ProtectedCreditCard.save` instead."
+      ProtectedCreditCard.save(params)
     end
 
-    def self.check_just_click_shop_params(params)
-      just_click_shop_attributes = [:request_id, :customer_name, :order_id, :amount, :payment_method,
-      :number_installments, :payment_type, :just_click_key, :security_code]
-
-      just_click_shop_attributes.each do |param|
-        raise IncompleteParams if params[param].nil?
-      end
-
-      raise InvalidSecurityCode if params[:security_code].to_s.size < 1 || params[:security_code].to_s.size > 4
-
-      raise InvalidNumberInstallments if params[:number_installments].to_i < 1 || params[:number_installments].to_i > 99
-
+    # <b>DEPRECATED:</b> Please use <tt>ProtectedCreditCard.get</tt> instead.
+    def self.get(just_click_key)
+      warn "[DEPRECATION] `CreditCard.get` is deprecated.  Please use `ProtectedCreditCard.get` instead."
+      ProtectedCreditCard.get(just_click_key)
     end
 
-    def self.valid_just_click_key?(just_click_key)
-      (just_click_key.is_a?(String) && just_click_key.size == 36)
+    # <b>DEPRECATED:</b> Please use <tt>ProtectedCreditCard.just_click_shop</tt> instead.
+    def self.just_click_shop(params = {})
+      warn "[DEPRECATION] `CreditCard.just_click_shop` is deprecated.  Please use `ProtectedCreditCard.just_click_shop` instead."
+      ProtectedCreditCard.just_click_shop(params)
     end
 
     def self.info_url
@@ -338,18 +176,6 @@ module Braspag
 
     def self.cancellation_url
       Braspag::Connection.instance.braspag_url + CANCELLATION_URI
-    end
-
-    def self.save_protected_card_url
-      Braspag::Connection.instance.protected_card_url + SAVE_PROTECTED_CARD_URI
-    end
-
-    def self.get_protected_card_url
-      Braspag::Connection.instance.protected_card_url + GET_PROTECTED_CARD_URI
-    end
-
-    def self.just_click_shop_url
-      Braspag::Connection.instance.protected_card_url + JUST_CLICK_SHOP_URI
     end
   end
 end
--- spec/credit_card_spec.rb
+++ spec/credit_card_spec.rb
@@ -1,12 +1,9 @@
-require 'ostruct'
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'ostruct'
 
 describe Braspag::CreditCard do
   let(:braspag_homologation_url) { "https://homologacao.pagador.com.br" }
   let(:braspag_production_url) { "https://transaction.pagador.com.br" }
-  let(:braspag_homologation_protected_card_url) { "https://cartaoprotegido.braspag.com.br" }
-  let(:braspag_production_protected_card_url) { "https://www.cartaoprotegido.com.br" }
   let(:merchant_id) { "um id qualquer" }
   let(:logger) { mock(:info => nil) }
 
@@ -229,262 +226,6 @@ describe Braspag::CreditCard do
     end
   end
 
-  describe ".save" do
-    let(:params) do
-      {
-        :customer_name => "W" * 21,
-        :holder =>  "Joao Maria Souza",
-        :card_number => "9" * 10,
-        :expiration => "10/12",
-        :order_id => "um order id",
-        :request_id => "00000000-0000-0000-0000-000000000044"
-      }
-    end
-
-    let(:params_with_merchant_id) do
-      params.merge!(:merchant_id => merchant_id)
-    end
-
-    let(:save_protected_card_url) { "http://braspag.com/bla" }
-
-    let(:savon_double) { double('Savon') }
-
-    before do
-      @connection.should_receive(:merchant_id)
-    end
-
-    context "with valid params" do
-      let(:valid_hash) do
-        {
-          :save_credit_card_response => {
-            :save_credit_card_result => {
-              :just_click_key => 'SAVE-PROTECTED-CARD-TOKEN',
-              :success => true
-            }
-          }
-        }
-      end
-
-      let(:response) do
-        double('Response', :to_hash => valid_hash)
-      end
-
-      before do
-        Braspag::CreditCard.should_receive(:save_protected_card_url)
-        Braspag::CreditCard.should_receive(:check_protected_card_params)
-                           .and_return(true)
-        Savon::Client.should_receive(:new).and_return(savon_double)
-        savon_double.should_receive(:request).and_return(response)
-
-        @response = Braspag::CreditCard.save(params)
-      end
-
-      it "should return a Hash" do
-        @response.should be_kind_of Hash
-        @response.should == {
-          :just_click_key => "SAVE-PROTECTED-CARD-TOKEN",
-          :success => true
-        }
-      end
-    end
-
-    context "with invalid params" do
-      let(:invalid_hash) do
-        {
-          :save_credit_card_response => {
-            :save_credit_card_result => {
-              :just_click_key => nil,
-              :success => false
-            }
-          }
-        }
-      end
-
-      let(:response) do
-        double('Response', :to_hash => invalid_hash)
-      end
-
-      before do
-        Braspag::CreditCard.should_receive(:check_protected_card_params)
-        Braspag::CreditCard.should_receive(:save_protected_card_url)
-                            .and_return(true)
-        Savon::Client.should_receive(:new).and_return(savon_double)
-        savon_double.should_receive(:request).and_return(response)
-
-        @response = Braspag::CreditCard.save(params)
-      end
-
-      it "should return a Hash" do
-        @response.should be_kind_of Hash
-        @response.should == {
-          :just_click_key => nil,
-          :success => false
-        }
-      end
-    end
-  end
-
-  describe ".get" do
-    let(:get_protected_card_url) { "http://braspag/bla" }
-
-    let(:invalid_xml) do
-      <<-EOXML
-      <CartaoProtegidoReturn xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                   xmlns="http://www.pagador.com.br/">
-        <CardHolder>Joao Maria Souza</CardHolder>
-        <CardNumber></CardNumber>
-        <CardExpiration>10/12</CardExpiration>
-        <MaskedCardNumber>******9999</MaskedCardNumber>
-      </CartaoProtegidoReturn>
-      EOXML
-    end
-
-    let(:valid_xml) do
-      <<-EOXML
-      <CartaoProtegidoReturn xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-                   xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-                   xmlns="http://www.pagador.com.br/">
-        <CardHolder>Joao Maria Souza</CardHolder>
-        <CardNumber>9999999999</CardNumber>
-        <CardExpiration>10/12</CardExpiration>
-        <MaskedCardNumber>******9999</MaskedCardNumber>
-      </CartaoProtegidoReturn>
-      EOXML
-    end
-
-    it "should raise an error when just click key is not valid" do
-      Braspag::CreditCard.should_receive(:valid_just_click_key?)
-                         .with("bla")
-                         .and_return(false)
-
-      expect {
-        Braspag::CreditCard.get "bla"
-      }.to raise_error(Braspag::InvalidJustClickKey)
-    end
-
-    it "should raise an error when Braspag returned an invalid xml as response" do
-      FakeWeb.register_uri(:post, get_protected_card_url, :body => invalid_xml)
-
-      Braspag::CreditCard.should_receive(:get_protected_card_url)
-                         .and_return(get_protected_card_url)
-
-      expect {
-        Braspag::CreditCard.get("b0b0b0b0-bbbb-4d4d-bd27-f1f1f1ededed")
-      }.to raise_error(Braspag::UnknownError)
-    end
-
-    it "should return a Hash when Braspag returned a valid xml as response" do
-      FakeWeb.register_uri(:post, get_protected_card_url, :body => valid_xml)
-
-      Braspag::CreditCard.should_receive(:get_protected_card_url)
-                         .and_return(get_protected_card_url)
-
-      response = Braspag::CreditCard.get("b0b0b0b0-bbbb-4d4d-bd27-f1f1f1ededed")
-      response.should be_kind_of Hash
-
-      response.should == {
-        :holder => "Joao Maria Souza",
-        :expiration => "10/12",
-        :card_number => "9" * 10,
-        :masked_card_number => "*" * 6 + "9" * 4
-      }
-    end
-
-  end
-
-  describe ".just_click_shop" do
-    context "body" do
-      let(:params) { {
-        :request_id => "123",
-        :customer_name => "Joao Silva",
-        :order_id => "999",
-        :amount => 10.50,
-        :payment_method => :redecard,
-        :number_installments => 3,
-        :payment_type => "test",
-        :just_click_key => "key",
-        :security_code => "123"
-      } }
-
-      class SavonClientTest
-        attr_accessor :response
-        attr_reader :method
-
-        def request(web, method, &block)
-          @method = method
-          instance_eval &block
-
-          @response
-        end
-
-        def soap
-          @soap ||= OpenStruct.new
-        end
-      end
-
-      before :each do
-        @savon_client_test = SavonClientTest.new
-        @savon_client_test.response = {:just_click_shop_response => {}}
-        Savon::Client.stub(:new).with('https://www.cartaoprotegido.com.br/Services/TestEnvironment/CartaoProtegido.asmx?wsdl').and_return(@savon_client_test)
-      end
-
-      after :each do
-        Savon::Client.unstub(:new)
-      end
-
-      it "should have RequestId" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['RequestId'].should eq '123'
-      end
-
-      it "should have MerchantKey" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['MerchantKey'].should eq 'um id qualquer'
-      end
-
-      it "should have CustomerName" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['CustomerName'].should eq 'Joao Silva'
-      end
-
-      it "should have OrderId" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['OrderId'].should eq '999'
-      end
-
-      it "should have Amount" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['Amount'].should eq 10.50
-      end
-
-      it "should have PaymentMethod" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['PaymentMethod'].should eq 20
-      end
-
-      it "should have PaymentType" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['PaymentType'].should eq 'test'
-      end
-
-      it "should have NumberInstallments" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['NumberInstallments'].should eq 3
-      end
-
-      it "should have JustClickKey" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['JustClickKey'].should eq 'key'
-      end
-
-      it "should have SecurityCode" do
-        described_class.just_click_shop(params)
-        @savon_client_test.soap.body['justClickShopRequestWS']['SecurityCode'].should eq '123'
-      end
-    end
-  end
-
   describe ".info" do
     let(:info_url) { "http://braspag/bla" }
 
@@ -682,14 +423,6 @@ describe Braspag::CreditCard do
       Braspag::CreditCard.cancellation_url.should == "#{braspag_homologation_url}/webservices/pagador/Pagador.asmx/VoidTransaction"
     end
 
-    it ".save_protected_card_url .get_protected_card_url" do
-      @connection.stub(:protected_card_url => braspag_homologation_protected_card_url)
-
-      Braspag::CreditCard.save_protected_card_url.should == "#{braspag_homologation_protected_card_url}/CartaoProtegido.asmx?wsdl"
-      Braspag::CreditCard.get_protected_card_url.should == "#{braspag_homologation_protected_card_url}/CartaoProtegido.asmx/GetCreditCard"
-    end
-
-
     it "should return the correct credit card creation url when connection environment is production" do
       @connection.stub(:braspag_url => braspag_production_url)
 
@@ -697,12 +430,18 @@ describe Braspag::CreditCard do
       Braspag::CreditCard.capture_url.should == "#{braspag_production_url}/webservices/pagador/Pagador.asmx/Capture"
       Braspag::CreditCard.cancellation_url.should == "#{braspag_production_url}/webservices/pagador/Pagador.asmx/VoidTransaction"
     end
+  end
 
-    it "should return the correct protected credit card url when connection environment is production" do
-      @connection.stub(:protected_card_url => braspag_production_protected_card_url)
+  describe ".save .get .just_click_shop" do
+    it "should delegate to ProtectedCreditCard class" do
+      Braspag::ProtectedCreditCard.should_receive(:save).with({})
+      Braspag::CreditCard.save({})
 
-      Braspag::CreditCard.save_protected_card_url.should == "#{braspag_production_protected_card_url}/CartaoProtegido.asmx?wsdl"
-      Braspag::CreditCard.get_protected_card_url.should == "#{braspag_production_protected_card_url}/CartaoProtegido.asmx/GetCreditCard"
+      Braspag::ProtectedCreditCard.should_receive(:get).with('just_click_key')
+      Braspag::CreditCard.get('just_click_key')
+
+      Braspag::ProtectedCreditCard.should_receive(:just_click_shop).with({})
+      Braspag::CreditCard.just_click_shop({})
     end
   end
 end
